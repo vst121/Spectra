5a2cfc3b5484078445728489f151ee78b8e4e3ab
Vahid Saadat <vst121@gmail.com>
2025-11-05 10:33:45 +0100

    Add OpenCV

---
+++ Spectra.ImageProcessing/Processing/AgeGenderOpenCvService.cs
@@ -0,0 +1,84 @@
+﻿namespace Spectra.ImageProcessing.Processing;
+
+using OpenCvSharp;
+using OpenCvSharp.Dnn;
+
+public class AgeGenderOpenCvService : IDisposable
+{
+    public AgeGenderOpenCvService()
+    {
+    }
+
+    public void PredictAgeGender(Stream imageStream)
+    {
+        var path = @"D:\DotNetProjects10\Spectra\Spectra.ImageProcessing\Models\AgeGenderOpenCV\";
+
+        string ageProto = Path.Combine(path, "age_deploy.prototxt");
+        string ageModel = Path.Combine(path, "age_net.caffemodel");
+        string genderProto = Path.Combine(path, "gender_deploy.prototxt");
+        string genderModel = Path.Combine(path, "gender_net.caffemodel");
+        var faceCascade = new CascadeClassifier(Path.Combine(path, "haarcascade_frontalface_default.xml"));
+
+        string[] ageList = ["0-2", "4-6", "8-12", "15-20", "25-32", "38-43", "48-53", "60+"];
+        string[] genderList = ["Male", "Female"];
+
+        var ageNet = CvDnn.ReadNetFromCaffe(ageProto, ageModel);
+        var genderNet = CvDnn.ReadNetFromCaffe(genderProto, genderModel);
+
+        //// Load the image
+        //var imagePath = @"D:\DotNetProjects10\Spectra\Spectra.BlazorApp\TestImages\";
+        //Mat frame = Cv2.ImRead(imagePath + "test.jpg");
+
+        byte[] imageBytes;
+        using (var ms = new MemoryStream())
+        {
+            imageStream.CopyTo(ms);
+            imageBytes = ms.ToArray();
+        }
+
+        using Mat imgMat = Cv2.ImDecode(imageBytes, ImreadModes.Color);
+        Mat frame = imgMat.Clone();
+
+        var faces = faceCascade.DetectMultiScale(frame, 1.1, 5, HaarDetectionTypes.ScaleImage);
+
+        foreach (var face in faces)
+        {
+            Mat faceROI = new(frame, face);
+            Mat blob = CvDnn.BlobFromImage(faceROI, 1.0, new Size(227, 227),
+                                           new Scalar(78.4263377603, 87.7689143744, 114.895847746), false);
+            //Mat blob = CvDnn.BlobFromImage(faceROI, 1.0, new Size(227, 227), new Scalar(104, 117, 123), false);
+
+            // Predict gender
+            genderNet.SetInput(blob);
+            var genderPreds = genderNet.Forward();
+
+            OpenCvSharp.Point minLoc, maxLoc;
+            double minVal, maxVal;
+
+            Cv2.MinMaxLoc(genderPreds, out minVal, out maxVal, out minLoc, out maxLoc);
+            int genderIndex = (int)maxLoc.X;
+            string gender = genderList[genderIndex];
+
+            // Predict age
+            ageNet.SetInput(blob);
+            var agePreds = ageNet.Forward();
+
+            Cv2.MinMaxLoc(agePreds, out minVal, out maxVal, out minLoc, out maxLoc);
+            int ageIndex = (int)maxLoc.X;
+            string age = ageList[ageIndex];
+
+            // Draw result
+            Cv2.PutText(frame, $"{gender}, {age}", new Point(face.X, face.Y - 10),
+                        HersheyFonts.HersheySimplex, 0.8, Scalar.Red, 2);
+            Cv2.Rectangle(frame, face, Scalar.Green, 2);
+        }
+
+        Cv2.ImShow("Age and Gender Detection", frame);
+        Cv2.WaitKey(0);
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
--- Spectra.ImageProcessing/Processing/AgeGenderService.cs
+++ Spectra.ImageProcessing/Processing/AgeGenderService.cs
@@ -1,6 +1,9 @@
-﻿using SixLabors.ImageSharp.Advanced;
+﻿namespace Spectra.ImageProcessing.Processing;
 
-namespace Spectra.ImageProcessing.Processing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 public class AgeGenderService : IDisposable
 {
@@ -19,50 +22,15 @@ public class AgeGenderService : IDisposable
     }
 
 
-    public (float age, string gender, float genderConfidence) PredictAgeGender(Stream selectedImage)
+    public (string age, string gender, float genderConfidence) PredictAgeGender(Stream selectedImage)
     {
         // Preprocess image -> tensor
         var tensor = ImageToTensor(selectedImage, _inputWidth, _inputHeight);
 
-        float age, genderConf;
-        string gender;
+        var age = GetAge(tensor);
+        (var gender, float genderConfidence) = GetGender(tensor);
 
-        age = GetAge(tensor);
-        (gender, genderConf) = GetGender(tensor);
-
-        return (age, gender, genderConf);
-    }
-
-    public float GetAge(DenseTensor<float> tensor)
-    {
-        var inputMeta = _sessionForAge.InputMetadata;
-        var firstInputName = inputMeta.Keys.First();
-
-        var inputs = new List<NamedOnnxValue>
-        {
-            NamedOnnxValue.CreateFromTensor(firstInputName, tensor)
-        };
-
-        using var results = _sessionForAge.Run(inputs);
-
-        var outList = results.ToList();
-
-        float age = -1f;
-
-        if (outList.Count == 1 && outList[0].Value is DenseTensor<float> t)
-        {
-            var values = t.ToArray();
-
-            if (values.Length == 8)
-            {
-                var ageProbs = Softmax(values);
-                var maxIndex = Array.IndexOf(ageProbs, ageProbs.Max());
-                int[] ageMidpoints = [1, 5, 10, 18, 28, 40, 50, 65];
-                age = ageMidpoints[maxIndex];
-            }
-        }
-
-        return age;
+        return (age, gender, genderConfidence);
     }
 
     public (string gender, float genderConfidence) GetGender(DenseTensor<float> tensor)
@@ -97,6 +65,48 @@ public class AgeGenderService : IDisposable
         return (gender, genderConf);
     }
 
+    public string GetAge(DenseTensor<float> tensor)
+    {
+        var inputMeta = _sessionForAge.InputMetadata;
+        var firstInputName = inputMeta.Keys.First();
+
+        var inputs = new List<NamedOnnxValue>
+        {
+            NamedOnnxValue.CreateFromTensor(firstInputName, tensor)
+        };
+
+        using var results = _sessionForAge.Run(inputs);
+
+        var outList = results.ToList();
+
+        string age = "Unknown!";
+
+        if (outList.Count == 1 && outList[0].Value is DenseTensor<float> t)
+        {
+            var values = t.ToArray();
+
+            if (values.Length == 8)
+            {
+                var ageProbs = Softmax(values);
+
+                float[] ageCenters = [1, 5, 10, 18, 28, 40, 50, 80];
+                string[] ageRanges = ["0 - 2", "4 - 6", "8 - 13", "15 - 20", "25 - 32", "38 - 43", "48 - 53", "60 - 100"];
+
+                float predictedAge = 0f;
+                for (int i = 0; i < 8; i++)
+                    predictedAge += ageProbs[i] * ageCenters[i];
+
+                var maxIndex = Array.IndexOf(ageProbs, ageProbs.Max());
+                string predictedRange = ageRanges[maxIndex];
+
+                age = $"{predictedRange} (≈ {predictedAge:F1} years)";
+            }
+        }
+
+        return age;
+    }
+
+
     private static float[] Softmax(float[] logits)
     {
         var max = logits.Max();
@@ -105,41 +115,39 @@ public class AgeGenderService : IDisposable
         return [.. exps.Select(x => x / sum)];
     }
 
-    private static DenseTensor<float> ImageToTensor(Stream selectedImage, int width, int height)
+    private static DenseTensor<float> ImageToTensor(
+        Stream imageStream, int width, int height,
+        bool useImageNetNormalization = false)
     {
-        using var img = Image.Load<Rgb24>(selectedImage);
-        img.Mutate(x => x.Resize(new ResizeOptions { Size = new SixLabors.ImageSharp.Size(width, height), Mode = ResizeMode.Crop }));
-
-
-        // Model normalization: many ONNX vision models expect (pixel - mean)/scale with mean around 127 and scale 255
-        // Adjust if the model README specifies other values.
-        float mean = 127f;
-        float scale = 255f;
-
+        using var image = Image.Load<Rgb24>(imageStream);
+        image.Mutate(x => x.Resize(new ResizeOptions
+        {
+            Size = new SixLabors.ImageSharp.Size(width, height),
+            Mode = ResizeMode.Crop
+        }));
 
         var tensor = new DenseTensor<float>([1, 3, height, width]);
 
+        float[] mean = useImageNetNormalization ? [0.485f, 0.456f, 0.406f] : [0f, 0f, 0f];
+        float[] std = useImageNetNormalization ? [0.229f, 0.224f, 0.225f] : [1f, 1f, 1f];
 
         for (int y = 0; y < height; y++)
         {
-            var span = img.Frames.RootFrame.DangerousGetPixelRowMemory(y).Span;
+            var pixelRow = image.DangerousGetPixelRowMemory(y).Span;
             for (int x = 0; x < width; x++)
             {
-                var p = span[x];
-                // channel order: R,G,B -> put into tensor as [0,R],[1,G],[2,B]
-                tensor[0, 0, y, x] = (p.R - mean) / scale;
-                tensor[0, 1, y, x] = (p.G - mean) / scale;
-                tensor[0, 2, y, x] = (p.B - mean) / scale;
+                ref var p = ref pixelRow[x];
+                tensor[0, 0, y, x] = ((p.B / 255f) - mean[0]) / std[0];
+                tensor[0, 1, y, x] = ((p.G / 255f) - mean[1]) / std[1];
+                tensor[0, 2, y, x] = ((p.R / 255f) - mean[2]) / std[2];
             }
         }
-
 
         return tensor;
     }
 
     public void Dispose()
     {
-        _sessionForAge.Dispose();
         _sessionForGender.Dispose();
         GC.SuppressFinalize(this);
     }
